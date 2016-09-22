e986d9ee0ec2254e4a5111600fa57a3bbd6d58c0
jerryyip <563355085@qq.com>
2016-09-22 08:10:48 +0800

    1

--- p16_navigating_trees.py
+++ p16_navigating_trees.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "lxml")
 
-#for child in bsObj.find("table", {"id":"giftList"}):
+#for child in bsObj.find("table", {"id":"giftList"}).children:
   
 for child in bsObj.find("table", {"id":"giftList"}).tr.next_siblings:
     print(child)
